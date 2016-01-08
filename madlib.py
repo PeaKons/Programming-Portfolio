a3af9a4cf813a02de833c566b7d72eccd30cbfb7
Erik Sanchez <eriksanchez@Alveyworld-20.local>
2016-01-08 08:41:02 -0700

    all the things

---
+++ madlib.py
@@ -0,0 +1,8 @@
+# madlib program to tell a silly story
+
+adj1 = raw_input("Enter an Adjective: ")
+adj2 = raw_input("Enter an Adjective: ")
+
+
+print "It was a " + adj1 + ", cold November day. I woke up to the " + adj2
+print "smell of "
